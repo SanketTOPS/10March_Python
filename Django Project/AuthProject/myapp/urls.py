442f5b1784dc7a15e70c34dbfddc943a3ee3f6eb
SanketTops <sanket.tops@gmail.com>
2023-06-12 11:01:59 +0530

    User Auth - Session

--- Django Project/AuthProject/myapp/urls.py
+++ Django Project/AuthProject/myapp/urls.py
@@ -5,4 +5,5 @@ from myapp import views
 urlpatterns = [
     path('',views.index),
     path('signup/',views.signup),
+    path('home/',views.home,name='home'),
 ]
--- Django Project/AuthProject/myapp/views.py
+++ Django Project/AuthProject/myapp/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render,redirect
 from .forms import signupForm
+from .models import userSignup
 
 # Create your views here.
 
 def index(request):
+    if request.method=='POST':
+        unm=request.POST['username']
+        pas=request.POST['password']
+
+        user=userSignup.objects.filter(username=unm,password=pas)
+        if user: #true
+            print("Login successfull!")
+            request.session['user']=unm #create a session
+            return redirect('home')
+        else:
+            print("Error!Incorrect username or password.")
     return render(request,'index.html')
 
 def signup(request):
@@ -16,3 +28,7 @@ def signup(request):
         else:
             print(newuser.errors)
     return render(request,'signup.html')
+
+def home(request):
+    user=request.session.get('user')
+    return render(request,'home.html',{'user':user})
