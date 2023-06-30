3cc835168286059d41a3921fef94cef3225ddd5b
SanketTops <sanket.tops@gmail.com>
2023-06-30 10:45:27 +0530

    Auth with Session

--- Django Project/NotesApp/myapp/views.py
+++ Django Project/NotesApp/myapp/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import signupForm
+from .models import userSignup
 
 # Create your views here.
 
 def index(request):
+    if request.method=='POST':
+        if request.POST.get('signup')=='signup':
+            newuser=signupForm(request.POST)
+            if newuser.is_valid():
+                newuser.save()
+                print("Signup Successfully!")
+                #return redirect('notes')
+            else:
+                print(newuser.errors)
+        elif request.POST.get('login')=='login':
+
+            unm=request.POST['username']
+            pas=request.POST['password']
+
+            user=userSignup.objects.filter(username=unm,password=pas)
+            if user:
+                print("Login Successfully!")
+                request.session['user']=unm
+                return redirect('notes')
+            else:
+                print("Error!Login fail....")
     return render(request,'index.html')
 
 def notes(request):
-    return render(request,'notes.html')
+    user=request.session.get('user')
+    return render(request,'notes.html',{'user':user})
 
 def profile(request):
     return render(request,'profile.html')
